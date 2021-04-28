f36bf6e1af7cf21a5017c162963bd86e6ffec76e
Sarah Ayanwale <sarahayanwale@gmail.com>
2021-04-28 10:21:11 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classes___Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
